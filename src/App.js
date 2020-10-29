eb901b64e13ec13926f29d33b3ce16504b3a07dd
tyagivipul629 <vipultyagi629@gmail.com>
2020-10-29 20:36:20 +0530

    Added Redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
+import {connect} from 'react-redux';
+
 import './App.css';
 
-function App() {
-	return <div className="App">Gratis Platform</div>;
+class App extends React.Component{
+	render(){return (<div className="App">
+		<button onClick={this.props.decrease}>Decrease</button>
+		<p>{this.props.number}</p>
+		<button onClick={this.props.increase}>Increase</button>
+		</div>);}
 }
 
-export default App;
+const mapDispatchToProps=(dispatch)=>{
+	return {
+		increase: ()=> dispatch({
+			type: 'INCREAMENT'
+		}),
+		decrease: ()=>dispatch({
+			type: 'DECREAMENT'
+		})
+	}
+}
+
+const mapstateToProps=(state)=>{
+	return {
+		number: state.numState.number
+	}
+}
+
+export default connect(mapstateToProps,mapDispatchToProps)(App);
