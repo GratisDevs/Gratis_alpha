b804f91d6e18373d7bf894a7bf2eca2299168241
tyagivipul629 <vipultyagi629@gmail.com>
2020-10-30 20:40:34 +0530

    Le Mar

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import React from 'react';
 
-import {connect} from 'react-redux';
-
 import './App.css';
 
 class App extends React.Component{
 	render(){return (<div className="App">
-		<button onClick={this.props.decrease}>Decrease</button>
-		<p>{this.props.number}palash</p>
-		<button onClick={this.props.increase}>Increase</button>
+		Gratis Platform
 		</div>);}
 }
 
-const mapDispatchToProps=(dispatch)=>{
-	return {
-		increase: ()=> dispatch({
-			type: 'INCREAMENT'
-		}),
-		decrease: ()=>dispatch({
-			type: 'DECREAMENT'
-		})
-	}
-}
 
-const mapstateToProps=(state)=>{
-	return {
-		number: state.numState.number
-	}
-}
-
-export default connect(mapstateToProps,mapDispatchToProps)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -4,26 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { combineReducers} from 'redux';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
 
 
-const numState=(state={number:0},action)=>{
-	switch(action.type){
-		case 'INCREAMENT': return {...state,number:state.number+1};
-		case 'DECREAMENT': return {...state,number:state.number-1};
-		default: return {...state};
-	}
-}
-
-const store= createStore(combineReducers({numState: numState}));
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Provider store={store}>
+		
 			<App />
-		</Provider>
+		
 	</React.StrictMode>,
 	document.getElementById('root')
 );
