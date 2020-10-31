0a761cc75498792d35c1c7c8b4e4c2d9f72af6e7
blaze021 <tarunblaze@gmail.com>
2020-10-31 20:14:04 +0530

    basic navbar added and fonts added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import NavbarMainComponent from './components/navbar_components/navbarmaincomponent';
 
-import './App.css';
-
-class App extends React.Component{
-	render(){return (<div className="App">
-		Gratis Platform
-		</div>);}
+class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<NavbarMainComponent />
+			</div>
+		);
+	}
 }
-
 
 export default App;
