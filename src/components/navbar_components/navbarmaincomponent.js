f2d6d6f50986e6e3796f964c86e5a2d77882ddb1
blaze021 <tarunblaze@gmail.com>
2020-11-02 00:11:29 +0530

    Navbar update

--- src/components/navbar_components/navbarmaincomponent.js
+++ src/components/navbar_components/navbarmaincomponent.js
@@ -5,18 +5,34 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 function NavbarMainComponent() {
 	return (
 		<div>
 			<Navbar bg="light" variant="light" className="navbrand-center">
-				<Nav.Link className="mr-2 active " href="/home">
+				<Nav.Link style={{ textDecoration: 'none' }} className="mr-2 active text-dark " href="/home">
 					Home
 				</Nav.Link>
+				{'  '}
+				<Dropdown as={ButtonGroup}>
+					<Button variant="light">Sub - Gratis Topics</Button>
+					<Dropdown.Toggle split variant="dark" id="dropdown-custom-2" />
+					<Dropdown.Menu className="super-colors">
+						<Dropdown.Item eventKey="1">Action</Dropdown.Item>
+						<Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+						<Dropdown.Item eventKey="3" active>
+							Active Item
+						</Dropdown.Item>
+						<Dropdown.Divider />
+						<Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+					</Dropdown.Menu>
+				</Dropdown>
 				<div className="mx-auto ">
 					<Navbar.Brand className="logo-name ">GratiS'</Navbar.Brand>
 				</div>
-				<Nav.Link className="mr-2 active " href="/login">
+				<Nav.Link style={{ textDecoration: 'none' }} className="mr-2 active  text-dark" href="/login">
 					Login
 				</Nav.Link>
 			</Navbar>
