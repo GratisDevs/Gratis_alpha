98c84942d552ecff55c184a8aa77dfd8b65cabbe
blaze021 <tarunblaze@gmail.com>
2020-11-01 23:31:47 +0530

    Navbar update

--- src/App.js
+++ src/App.js
@@ -1,55 +1,60 @@
 import React from 'react';
-import './App.css';
 import firebase from './components/data_components/firebase.js';
-import Login from './components/react-components/Login.js'
-import {connect} from 'react-redux';
-import {logout, login} from './actions/login_logout.js';
-import { Redirect, Route, Switch, withRouter} from 'react-router-dom';
+import Login from './components/react-components/Login.js';
+import { connect } from 'react-redux';
+import { logout, login } from './actions/login_logout.js';
+import { Redirect, Route, Switch, withRouter } from 'react-router-dom';
 import Homepage from './components/react-components/Homepage.js';
+import NavbarMainComponent from './components/navbar_components/navbarmaincomponent';
 
-
-class App extends React.Component{
-
-	constructor(props){
+class App extends React.Component {
+	constructor(props) {
 		super(props);
-		
-	}	
-	
-	componentDidMount(){
-		firebase.auth().onAuthStateChanged((user)=>{
-			if(user){
-				this.props.dispatch(login(user.email));	
-				this.props.history.push("/");
-			}
-			
-		})
-		
 	}
-	logout=()=>{
-		console.log("clicked");
-		firebase.auth().signOut().then(()=>{
-			this.props.dispatch(logout());
-			this.props.history.push("/login");
-		}).catch((err)=>{
-			console.log(err);
-		})
-	}
-	render(){
 
-		return (<div className="App">
-			<Switch>
-				<Route exact path="/" component={()=><Homepage userName={this.props.userName} logout={this.logout}/>} />
-				<Route exact path="/login" component={Login} />
-			</Switch>
-		</div>);}
+	componentDidMount() {
+		firebase.auth().onAuthStateChanged((user) => {
+			if (user) {
+				this.props.dispatch(login(user.email));
+				this.props.history.push('/');
+			}
+		});
+	}
+	logout = () => {
+		console.log('clicked');
+		firebase
+			.auth()
+			.signOut()
+			.then(() => {
+				this.props.dispatch(logout());
+				this.props.history.push('/login');
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+	render() {
+		return (
+			<div className="App">
+				<NavbarMainComponent />
+				<Switch>
+					<Route
+						exact
+						path="/"
+						component={() => <Homepage userName={this.props.userName} logout={this.logout} />}
+					/>
+					<Route exact path="/login" component={Login} />
+				</Switch>
+			</div>
+		);
+	}
 }
 
-const mapStateToProps=(state)=>{
-	return{
+const mapStateToProps = (state) => {
+	return {
 		isLoggedIn: state.userState.isLoggedIn,
 		userName: state.userState.userName
 	};
-}
-
+};
 
 export default withRouter(connect(mapStateToProps)(App));
--- src/components/navbar_components/navbarmaincomponent.js
+++ src/components/navbar_components/navbarmaincomponent.js
@@ -5,18 +5,18 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
 
 function NavbarMainComponent() {
 	return (
 		<div>
 			<Navbar bg="light" variant="light">
-				<Navbar.Brand className="logo-name">GratiS</Navbar.Brand>
+				<Nav.Link className="mr-2 active " href="/home">
+					Home
+				</Nav.Link>
+				<Navbar.Brand className="logo-name mx-auto">GratiS'</Navbar.Brand>
 				<Navbar.Collapse id="basic-navbar-nav" className="justify-content-end mr-5">
 					<div>
 						<Nav className="mr-auto">
-							<Nav.Link className="mr-2 active ">Home</Nav.Link>
 							<Nav.Link className="mr-2 ">Login</Nav.Link>
 						</Nav>
 					</div>
